1f18bacf4fca7098b5614748496799285898bb81
leonardo.goandete <leonardo.goandete@hotmail.com>
2025-06-10 13:27:05 -0300

    Add primeiro teste com testfy

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/leonardogoandete/go-gin-api/controllers"
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -19,7 +20,5 @@ func TestVerificaRetonoPingComSucesso(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/ping", nil)
 	resp := httptest.NewRecorder()
 	r.ServeHTTP(resp, req)
-	if resp.Code != http.StatusOK {
-		t.Errorf("Código de resposta esperado: %d, mas obteve: %d", http.StatusOK, resp.Code)
-	}
+	assert.Equal(t, http.StatusOK, resp.Code)
 }
