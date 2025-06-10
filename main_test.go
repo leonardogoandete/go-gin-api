ce4752c66df92bccd3a5efe5db5d9a2d8c1d4b53
leonardo.goandete <leonardo.goandete@hotmail.com>
2025-06-10 13:32:08 -0300

    Add primeiro teste com testfy

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/leonardogoandete/go-gin-api/controllers"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -21,4 +22,7 @@ func TestVerificaRetonoPingComSucesso(t *testing.T) {
 	resp := httptest.NewRecorder()
 	r.ServeHTTP(resp, req)
 	assert.Equal(t, http.StatusOK, resp.Code, "Status code deve ser 200 OK")
+	mockDaResposta := `{"message":"pong"}`
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal(t, mockDaResposta, string(respBody), "Resposta deve ser igual a "+mockDaResposta)
 }
